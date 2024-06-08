d298012aedc336d2f6f207c479bb518603c36f83
AlanMars <alan.yin@icloud.com>
2024-06-08 19:33:36 +0800

    Polish - set port 5003 for account-api services

--- racio-account/account-api/app.py
+++ racio-account/account-api/app.py
@@ -252,4 +252,4 @@ def pool_stat():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+    app.run(host='0.0.0.0', port=5003)
--- racio-account/account-api/docker/entrypoint.sh
+++ racio-account/account-api/docker/entrypoint.sh
@@ -11,10 +11,10 @@ fi
 pip install --prefix=/pkg -r requirements.txt
 
 if [[ "${DEBUG}" == "true" ]]; then
-  flask run --host=${ACCOUNT_BIND_ADDRESS:-0.0.0.0} --port=${ACCOUNT_PORT:-5002} --debug
+  flask run --host=${ACCOUNT_BIND_ADDRESS:-0.0.0.0} --port=${ACCOUNT_PORT:-5003} --debug
 else
   gunicorn \
-    --bind "${ACCOUNT_BIND_ADDRESS:-0.0.0.0}:${ACCOUNT_PORT:-5002}" \
+    --bind "${ACCOUNT_BIND_ADDRESS:-0.0.0.0}:${ACCOUNT_PORT:-5003}" \
     --workers ${SERVER_WORKER_AMOUNT:-1} \
     --worker-class ${SERVER_WORKER_CLASS:-gevent} \
     --timeout ${GUNICORN_TIMEOUT:-200} \
